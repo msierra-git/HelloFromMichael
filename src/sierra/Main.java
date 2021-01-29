9fce741d5adeb973642a6c425fe0a61acf12ca97
Alfred Ortega <jusalfort07@byui.edu>
2021-01-29 13:31:21 +0800

    Add my string to this project

--- src/sierra/Main.java
+++ src/sierra/Main.java
@@ -5,5 +5,8 @@ public class Main {
     public static void main(String[] args) {
 	    String msg = "Hello, my name is Michael";
 	    System.out.println(msg);
+
+	    String msg2 = "Hello Michael, my name is Alfred Ortega";
+	    System.out.println(msg2);
     }
 }
